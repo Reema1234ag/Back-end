48f7e3c48ec61f4665af41b5d0532f2af53b0f03
Reema1234ag <reema.agrawal_cs18@gla.ac.in>
2021-04-15 18:26:35 +0530

    home routing

--- express app/index.js
+++ express app/index.js
@@ -4,8 +4,19 @@ const express=require("express")
 const app=express();
 
 // generalize production PORT
-const PORT=process.env.PORT ||3000;
+const PORT=process.env.PORT || 3000;
 //3.listen server app on a PORT
 app.listen(PORT,function(){
     console.log(`Server started at PORT ${PORT}`);
 });
+//4. reply by the server to client(handle client request)
+//syntax->
+// app.method('route',callback(){})
+//there are mainly four type of methods i.e get,post,put,delete
+
+//handling '/' Home Router-------------------------
+app.get('/',function(req,res){
+    res.send("hello this is express server");
+})
+
+
